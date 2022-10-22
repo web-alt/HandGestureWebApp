ca82687ebfcb384283b19c0c067a083ae8d6208e
Mr. Unknown <84503682+web-alt@users.noreply.github.com>
2022-10-22 15:59:15 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,103 @@
+Webcam.set({
+    width:350,
+height:300,
+image_format : 'png' ,
+png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach(  '#camera'  );
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JcXivmWHn/model.json', modelLoaded);
+
+function  modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      document.getElementById("result_hand_gesture_name").innerHTML = results[0].label;
+  
+      if(results[0].label == "super")
+      {
+          document.getElementById("update_hand_gesture").innerHTML = "&#128076;";
+      }
+      if(results[0].label == "thumps_up")
+      {
+          document.getElementById("update_hand_gesture").innerHTML = "&#128077;";
+      }
+      if(results[0].label == "thumps_down")
+      {
+          document.getElementById("update_hand_gesture").innerHTML = "&#128078;";
+      }
+  
+    }
+  }Webcam.set({
+    width:350,
+height:300,
+image_format : 'png' ,
+png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach(  '#camera'  );
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JcXivmWHn/model.json', modelLoaded);
+
+function  modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      document.getElementById("result_hand_gesture_name").innerHTML = results[0].label;
+  
+      if(results[0].label == "super")
+      {
+          document.getElementById("update_hand_gesture").innerHTML = "&#128076;";
+      }
+      if(results[0].label == "thumps_up")
+      {
+          document.getElementById("update_hand_gesture").innerHTML = "&#128077;";
+      }
+      if(results[0].label == "thumps_down")
+      {
+          document.getElementById("update_hand_gesture").innerHTML = "&#128078;";
+      }
+  
+    }
+  }
